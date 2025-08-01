03754a689d9e66ed859824b1aa4abbcbf8ba3963
Ash Bullock <mondeendeguise@gmail.com>
2025-08-01 14:33:37 -0600

    fix: check appropriate buffer in is_alive
    rewrite swap_buffers to swap content rather than pointers
    modify build process for separate compilation

--- nob.c
+++ nob.c
@@ -1,12 +1,13 @@
 #define NOB_IMPLEMENTATION
 #include "./src/nob.h"
 
-#include <string.h>
-
 #define COMMON_CFLAGS "-Wall", "-Wextra", "-pedantic", "-ggdb"
 
 bool build_term(Nob_Cmd *cmd) {
 	bool result = true;
+
+	nob_log(NOB_INFO, "Building target term...");
+
 	if(!nob_mkdir_if_not_exists("build")) return 1;
 	nob_cmd_append(cmd, "clang", COMMON_CFLAGS, "-o", "build/game-of-life", "src/main.c", "-lncurses");
 	if(!nob_cmd_run_sync(*cmd)) result = false;
@@ -14,12 +15,27 @@ bool build_term(Nob_Cmd *cmd) {
 	return result;
 }
 
-bool build_wasm(Nob_Cmd *cmd) {
+bool build_wasm(Nob_Procs *procs, Nob_Cmd *cmd) {
 	bool result = true;
+
+	nob_log(NOB_INFO, "Building target wasm...");
+
 	if(!nob_mkdir_if_not_exists("wasm")) return 1;
-	nob_cmd_append(cmd, "clang", COMMON_CFLAGS, "--target=wasm32", "-fno-builtin", "-nostdlib", "-Wl,--allow-undefined", "-Wl,--no-entry", "-Wl,--export-all", "./src/game.c", "./src/prng.c", "-o", "./wasm/game.wasm");
+
+	nob_cmd_append(cmd, "clang", COMMON_CFLAGS, "--target=wasm32", "-fno-builtin", "-nostdlib", "-c", "./src/game.c", "-o", "./src/game.wasm.o");
+	nob_da_append(procs, nob_cmd_run_async(*cmd));
+	cmd->count = 0;
+
+	nob_cmd_append(cmd, "clang", COMMON_CFLAGS, "--target=wasm32", "-fno-builtin", "-nostdlib", "-c", "./src/prng.c", "-o", "./src/prng.wasm.o");
+	nob_da_append(procs, nob_cmd_run_async(*cmd));
+	cmd->count = 0;
+
+	if(!nob_procs_wait(*procs)) return false;
+
+	nob_cmd_append(cmd, "clang", COMMON_CFLAGS, "--target=wasm32", "-fno-builtin", "-nostdlib", "-Wl,--allow-undefined", "-Wl,--no-entry", "-Wl,--export-all", "./src/game.wasm.o", "./src/prng.wasm.o", "-o", "./wasm/game.wasm");
 	if(!nob_cmd_run_sync(*cmd)) result = false;
 	cmd->count = 0;
+
 	return result;
 }
 
@@ -40,7 +56,7 @@ int main(int argc, char **argv) {
 		if(strcmp(subcmd, "term") == 0) {
 			if(!build_term(&cmd)) return 1;
 		} else if(strcmp(subcmd, "wasm") == 0) {
-			if(!build_wasm(&cmd)) return 1;
+			if(!build_wasm(&procs, &cmd)) return 1;
 		} else {
 			nob_log(NOB_ERROR, "unrecognized subcommand `%s`", subcmd);
 			usage(program);
@@ -48,7 +64,7 @@ int main(int argc, char **argv) {
 		}
 	} else {
 		if(!build_term(&cmd)) return 1;
-		if(!build_wasm(&cmd)) return 1;
+		if(!build_wasm(&procs, &cmd)) return 1;
 	}
 
 	nob_cmd_free(cmd);
--- src/game.c
+++ src/game.c
@@ -24,15 +24,27 @@ struct game_canvas {
 };
 
 void swap_buffers(struct game_canvas *gc) {
-	uint32_t *tmp = (*gc).cells;
-	(*gc).cells = (*gc).framebuffer;
-	(*gc).framebuffer = tmp;
+	for(int y = 0; y < gc->height; ++y) {
+		for(int x = 0; x < gc->width; ++x) {
+			// uint32_t tmp = gc->cells[y * gc->width + x];
+			gc->cells[y * gc->width + x] = gc->framebuffer[y * gc->width + x];
+			// gc->framebuffer[y * gc->width + x] = tmp;
+		}
+	}
 }
 
-void clear_canvas(struct game_canvas gc) {
-	for(int y = 0; y < gc.height; ++y) {
-		for(int x = 0; x < gc.width; ++x) {
-			gc.cells[y * gc.width + x] = 0;
+void clear_canvas(struct game_canvas *gc) {
+	for(int y = 0; y < gc->height; ++y) {
+		for(int x = 0; x < gc->width; ++x) {
+			gc->cells[y * gc->width + x] = 0;
+		}
+	}
+}
+
+void clear_framebuffer(struct game_canvas *gc) {
+	for(int y = 0; y < gc->height; ++y) {
+		for(int x = 0; x < gc->width; ++x) {
+			gc->framebuffer[y * gc->width + x] = 0;
 		}
 	}
 }
@@ -40,30 +52,29 @@ void clear_canvas(struct game_canvas gc) {
 // byte order is reversed here for some reason idk
 #define ALIVE 0xFFFF0000
 #define DEAD  0x00000000
-#define CELL_LIVE(x, y) framebuffer[(y * WIDTH) + x] = ALIVE
-#define CELL_DEAD(x, y) framebuffer[(y * WIDTH) + x] = DEAD
+#define CELL_LIVE(gc, x, y) (gc)->framebuffer[(y * (gc)->width) + x] = ALIVE
+#define CELL_DEAD(gc, x, y) (gc)->framebuffer[(y * (gc)->width) + x] = DEAD
 
-bool is_alive(int x, int y) {
-	if(framebuffer[(y * WIDTH) + x]) return true;
-	return false;
+bool is_alive(struct game_canvas gc, int x, int y) {
+	return gc.cells[(y * gc.width) + x] == ALIVE;
 }
 
-int get_live_neighbors(int x, int y) {
+int get_live_neighbors(struct game_canvas gc, int x, int y) {
 	int total = 0;
 
 	bool up = y != 0;
-	bool down = y != HEIGHT-1;
+	bool down = y != gc.height-1;
 	bool left = x != 0;
-	bool right = x != WIDTH-1;
+	bool right = x != gc.width-1;
 
-	if(left && up && is_alive(x-1, y-1)) ++total;
-	if(left && is_alive(x-1, y)) ++total;
-	if(left && down && is_alive(x-1, y+1)) ++total;
-	if(up && is_alive(x, y-1)) ++total;
-	if(down && is_alive(x, y+1)) ++total;
-	if(right && up && is_alive(x+1, y-1)) ++total;
-	if(right && is_alive(x+1, y)) ++total;
-	if(right && down && is_alive(x+1, y+1)) ++total;
+	if(left && up && is_alive(gc, x-1, y-1)) ++total;
+	if(left && is_alive(gc, x-1, y)) ++total;
+	if(left && down && is_alive(gc, x-1, y+1)) ++total;
+	if(up && is_alive(gc, x, y-1)) ++total;
+	if(down && is_alive(gc, x, y+1)) ++total;
+	if(right && up && is_alive(gc, x+1, y-1)) ++total;
+	if(right && is_alive(gc, x+1, y)) ++total;
+	if(right && down && is_alive(gc, x+1, y+1)) ++total;
 
 	return total;
 }
@@ -75,13 +86,13 @@ struct game_canvas randomize(unsigned int seed) {
 	// clear
 	for(int y = 0; y < gc.height; ++y) {
 		for(int x = 0; x < gc.width; ++x) {
-			CELL_DEAD(x, y);
+			CELL_DEAD(&gc, x, y);
 		}
 	}
 
 	for(int y = 0; y < gc.height; ++y) {
 		for(int x = 0; x < gc.width; ++x) {
-			if(randf() > 0.95f) CELL_LIVE(x, y);
+			if(randf() > 0.9f) CELL_LIVE(&gc, x, y);
 		}
 	}
 
@@ -92,17 +103,18 @@ struct game_canvas randomize(unsigned int seed) {
 struct game_canvas render(void) {
 	struct game_canvas gc = { cells, framebuffer, WIDTH, HEIGHT };
 
-	for(int y = 0; y < HEIGHT; ++y) {
-		for(int x = 0; x < WIDTH; ++x) {
-			swap_buffers(&gc);
-			int live_neighbors = get_live_neighbors(x, y);
-			if(is_alive(x, y)) {
-				if(live_neighbors > 1 && live_neighbors < 4) CELL_LIVE(x, y);
-				else CELL_DEAD(x, y);
+	clear_framebuffer(&gc);
+
+	for(int y = 0; y < gc.height; ++y) {
+		for(int x = 0; x < gc.width; ++x) {
+			int live_neighbors = get_live_neighbors(gc, x, y);
+			if(is_alive(gc, x, y)) {
+				if(live_neighbors < 2 || live_neighbors > 3) CELL_DEAD(&gc, x, y);
+				else CELL_LIVE(&gc, x, y);
 			} else {
-				if(live_neighbors == 3) CELL_LIVE(x, y);
+				if(live_neighbors == 3) CELL_LIVE(&gc, x, y);
+				else CELL_DEAD(&gc, x, y);
 			}
-			swap_buffers(&gc);
 		}
 	}
 
